d24b1ab8606ba248b7840b9a907faab9d15680d9
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2026-01-20 19:39:50 +0800

    feat(utils): [TSDK-040] guard touchsdk hook

--- packages/utils/plugin/sdk/touch-sdk.ts
+++ packages/utils/plugin/sdk/touch-sdk.ts
@@ -1,12 +1,18 @@
+import { hasWindow } from '../../env'
 import type { ITouchSDK } from './index'
 
 const DEFAULT_TOUCH_SDK_ERROR
   = '[TouchSDK] Touch SDK not available. Make sure this is called in a plugin context.'
 
+let cachedTouchSDK: ITouchSDK | null = null
+
 export function useTouchSDK(errorMessage: string = DEFAULT_TOUCH_SDK_ERROR): ITouchSDK {
-  const sdk = (window as any)?.$touchSDK as ITouchSDK | undefined
+  const globalWindow = hasWindow() ? window : undefined
+  const windowSdk = (globalWindow as any)?.$touchSDK as ITouchSDK | undefined
+  const sdk = windowSdk ?? cachedTouchSDK
   if (!sdk) {
     throw new Error(errorMessage)
   }
+  cachedTouchSDK = sdk
   return sdk
 }
