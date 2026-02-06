d1323d5b7fcbb52130f1c867674b38eb0a9f4cd8
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2026-02-06 22:57:08 +0800

    chore(stash): restore prep 2

---
+++ apps/nexus/app/components/content/demos/InputClearableDemo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const { locale } = useI18n()
+const clearableValue = ref('')
+</script>
+
+<template>
+  <div v-if="locale === 'zh'">
+        <TuffInput v-model="clearableValue" clearable placeholder="可清空" />
+</div>
+  <div v-else>
+        <TuffInput v-model="clearableValue" clearable placeholder="Clearable" />
+</div>
+</template>
