c290e5a5fe83e5e7e852e65444d0dc80606a81e2
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2026-02-06 22:53:02 +0800

    chore(stash): restore prep 3

---
+++ apps/nexus/app/components/content/demos/InputBasicInputDemo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const { locale } = useI18n()
+const value = ref('')
+</script>
+
+<template>
+  <div v-if="locale === 'zh'">
+        <TuffInput v-model="value" placeholder="请输入内容" />
+</div>
+  <div v-else>
+        <TuffInput v-model="value" placeholder="Type here..." />
+</div>
+</template>
