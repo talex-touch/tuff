9af252c33bc15f09b564ef88cc438f51b0a7d852
TalexDreamSoul <TalexDreamSoul@Gmail.com>
2026-02-06 19:03:05 +0800

    chore(stash): restore post-merge residual 1

--- apps/nexus/app/components/content/demos/ButtonLoadingDemo.vue
+++ apps/nexus/app/components/content/demos/ButtonLoadingDemo.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = withDefaults(defineProps<{
   loadingLabel?: string
   idleLabel?: string
 }>(), {
-  loadingLabel: 'Loading',
-  idleLabel: 'Click to load',
+  loadingLabel: '',
+  idleLabel: '',
 })
 
+const { locale } = useI18n()
 const loading = ref(false)
+
+const labels = computed(() => ({
+  loading: props.loadingLabel || (locale.value === 'zh' ? '加载中' : 'Loading'),
+  idle: props.idleLabel || (locale.value === 'zh' ? '点击加载' : 'Click to load'),
+}))
 
 async function handleClick() {
   if (loading.value)
@@ -23,12 +29,12 @@ async function handleClick() {
 <template>
   <div class="tuff-demo-row">
     <TxButton variant="primary" :loading="loading" @click="handleClick">
-      <span v-if="loading">{{ props.loadingLabel }}</span>
-      <span v-else>{{ props.idleLabel }}</span>
+      <span v-if="loading">{{ labels.loading }}</span>
+      <span v-else>{{ labels.idle }}</span>
     </TxButton>
     <TxButton variant="secondary" :loading="loading" @click="handleClick">
-      <span v-if="loading">{{ props.loadingLabel }}</span>
-      <span v-else>{{ props.idleLabel }}</span>
+      <span v-if="loading">{{ labels.loading }}</span>
+      <span v-else>{{ labels.idle }}</span>
     </TxButton>
     <TxButton circle icon="i-carbon-edit" :loading="loading" @click="handleClick" />
   </div>
